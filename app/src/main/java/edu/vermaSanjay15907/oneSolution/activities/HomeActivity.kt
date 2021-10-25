012623313ddfe9d738dabb231dcc2e6e6df23093
Sanjay Verma <vermasanjay15907@gmail.com>
2021-10-25 08:41:35 +0530

    Implemented Splash Activity

--- app/src/main/java/edu/vermaSanjay15907/oneSolution/activities/HomeActivity.kt
+++ app/src/main/java/edu/vermaSanjay15907/oneSolution/activities/HomeActivity.kt
@@ -1,7 +1,8 @@
-package edu.vermaSanjay15907.oneSolution
+package edu.vermaSanjay15907.oneSolution.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.vermaSanjay15907.oneSolution.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/edu/vermaSanjay15907/oneSolution/activities/SplashActivity.kt
@@ -0,0 +1,30 @@
+package edu.vermaSanjay15907.oneSolution.activities
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import edu.vermaSanjay15907.oneSolution.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 1000)
+    }
+}
