2c169dae890b15108f29e43a6d2388369b51a777
Sanjay Verma <vermasanjay15907@gmail.com>
2021-10-25 08:53:53 +0530

    Implemented Login Fragment

---
+++ app/src/main/java/edu/vermaSanjay15907/oneSolution/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package edu.vermaSanjay15907.oneSolution.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.vermaSanjay15907.oneSolution.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/edu/vermaSanjay15907/oneSolution/fragments/LoginFragment.kt
@@ -0,0 +1,19 @@
+package edu.vermaSanjay15907.oneSolution.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import edu.vermaSanjay15907.oneSolution.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
