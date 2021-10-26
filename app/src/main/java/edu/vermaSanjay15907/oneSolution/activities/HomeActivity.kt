a878559607a1b0d90aedbe7a8be95db633aad39e
Sanjay Verma <vermasanjay15907@gmail.com>
2021-10-26 20:26:26 +0530

    Implemented nav-drawer

--- app/src/main/java/edu/vermaSanjay15907/oneSolution/activities/HomeActivity.kt
+++ app/src/main/java/edu/vermaSanjay15907/oneSolution/activities/HomeActivity.kt
@@ -2,11 +2,38 @@ package edu.vermaSanjay15907.oneSolution.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import edu.vermaSanjay15907.oneSolution.R
+import edu.vermaSanjay15907.oneSolution.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        drawerLayout = binding.drawerLayout
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.homeActivityFragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            if (destination.id == controller.graph.startDestination)
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            else
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
